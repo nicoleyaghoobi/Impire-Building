7ad3d29108299247ce10740f3fffa1512efbb747
nicoleyaghoobi <53989251+nicoleyaghoobi@users.noreply.github.com>
2019-11-25 20:25:23 -0500

    Add project files.

---
+++ Controllers/VisitorsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ImpireBuilding.Models;
+using ImpireBuilding.Data;
+
+namespace ImpireBuilding.Controllers
+{
+    public class VisitorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VisitorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Visitors
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Visitor.Include(v => v.Parking);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Visitors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var visitor = await _context.Visitor
+                .Include(v => v.Parking)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (visitor == null)
+            {
+                return NotFound();
+            }
+
+            return View(visitor);
+        }
+
+        // GET: Visitors/Create
+        public IActionResult Create()
+        {
+            ViewData["ParkingId"] = new SelectList(_context.Set<Parking>(), "ParkingId", "ParkingId");
+            return View();
+        }
+
+        // POST: Visitors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,PhoneNumber,TimeOfEntry,TimeOfExit,UnitNumber,ParkingId")] Visitor visitor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(visitor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParkingId"] = new SelectList(_context.Set<Parking>(), "ParkingId", "ParkingId", visitor.ParkingId);
+            return View(visitor);
+        }
+
+        // GET: Visitors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var visitor = await _context.Visitor.FindAsync(id);
+            if (visitor == null)
+            {
+                return NotFound();
+            }
+            ViewData["ParkingId"] = new SelectList(_context.Set<Parking>(), "ParkingId", "ParkingId", visitor.ParkingId);
+            return View(visitor);
+        }
+
+        // POST: Visitors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,PhoneNumber,TimeOfEntry,TimeOfExit,UnitNumber,ParkingId")] Visitor visitor)
+        {
+            if (id != visitor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(visitor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VisitorExists(visitor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParkingId"] = new SelectList(_context.Set<Parking>(), "ParkingId", "ParkingId", visitor.ParkingId);
+            return View(visitor);
+        }
+
+        // GET: Visitors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var visitor = await _context.Visitor
+                .Include(v => v.Parking)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (visitor == null)
+            {
+                return NotFound();
+            }
+
+            return View(visitor);
+        }
+
+        // POST: Visitors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var visitor = await _context.Visitor.FindAsync(id);
+            _context.Visitor.Remove(visitor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VisitorExists(int id)
+        {
+            return _context.Visitor.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ImpireBuilding.Models;
+
+namespace ImpireBuilding.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Visitor> Visitor { get; set; }
+        public DbSet<Parking> Parking { get; set; }
+    }
+}
